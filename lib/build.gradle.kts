bfa9db1fa421f593d8a082854ba39dcdcddcd01a
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-04-08 21:35:23 -0700

    Initial project setup with Gradle/Kotlin multi-module structure

---
+++ lib/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
